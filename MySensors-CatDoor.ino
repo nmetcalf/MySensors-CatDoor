cc2b0acb06890d06d37d9c9b152ce198afa26c74
NdmGuru <ndm@ndm.guru>
2018-04-29 21:28:05 +1000

    General Improvements
    Changed to door sensor type. This should give us the ability to use the door image on the status. Also updated the state checking to be abit cleaner. Fixed door close timing. Removed not needed obstruction code. Code Clean up (old vars)

--- MySensors-CatDoor.ino
+++ MySensors-CatDoor.ino
@@ -14,25 +14,27 @@
 #define CHILD_ID_OPENER 0
 #define CHILD_ID_STATUS 1
 
-#define UPPER_LIMIT_SENSOR 2  //Pin used for input from the garage door upper limit sensor
-#define LOWER_LIMIT_SENSOR 3  //Pin used for input from the garage door lower limit sensor
+// Sensor Pins
+#define UPPER_LIMIT_SENSOR 3  //Pin used for input from the garage door upper limit sensor
+#define LOWER_LIMIT_SENSOR 2  //Pin used for input from the garage door lower limit sensor
+
+// Motor Pins
 #define DOOR_ACTUATOR_RELAY_OPEN 4 //Pin used to toggle the door actuator relay in
 #define DOOR_ACTUATOR_RELAY_CLOSE 5 //Pin used to toggle the door actuator relay out
+
+// HIGH or LOW logic?
 #define RELAY_ON 0            // GPIO value to write to turn on attached relay
 #define RELAY_OFF 1           // GPIO value to write to turn off attached relay
 
-#define TOGGLE_INTERVAL 1500  //Tells how many milliseconds the relay will be held closed
-#define HEARTBEAT_INTERVAL 120000 //Number of miliseconds before the next heartbeat
+#define HEARTBEAT_INTERVAL 300000 //Number of miliseconds before the next heartbeat
 
+// State int's
 #define CLOSED 0
 #define OPEN 1
-#define CLOSING 2
-#define OPENING 3
 
-#define MOTOR_OVERRUN_CLOSE 1000 // How long to overrun on open/close millis
-#define MOTOR_OVERRUN_OPEN  10 // How long to overrun on open/close millis
-
-#define OBSTR_MAX 20
+// Motor Overrun times (millis())
+#define MOTOR_OVERRUN_CLOSE 1200
+#define MOTOR_OVERRUN_OPEN  700
 
 const char *currentState[] = { 
                              "Closed", 
@@ -48,18 +50,12 @@ int cState;
 int lState;     
 
 //The last full limit position of the door (open or closed)
-int lLimit = 0; 
-
-//Used for the error condition when the door is unexpectedly reversed
-boolean obstruction = false;
-
-int obstruction_count = 0;
 
 // the timer object used for the heartbeat
 SimpleTimer heartbeat;
 
 MyMessage msgOpener(CHILD_ID_OPENER, V_STATUS);
-MyMessage msgStatus(CHILD_ID_STATUS, V_TEXT);
+MyMessage msgStatus(CHILD_ID_STATUS, V_TRIPPED);
 
 /**
  * presentation - Present the garage door sensor
@@ -70,7 +66,7 @@ void presentation() {
   
   // Register the garage door sensor with the gateway
   present( CHILD_ID_OPENER, S_BINARY );
-  present( CHILD_ID_STATUS, S_INFO );
+  present( CHILD_ID_STATUS, S_DOOR );
   
 } //End presentation
 
@@ -82,13 +78,16 @@ void setup() {
   // Set up the pins for reading the upper and lower limit sensors
   pinMode(UPPER_LIMIT_SENSOR, INPUT);
   pinMode(LOWER_LIMIT_SENSOR, INPUT);
+  
   // Set up the pin to control the door opener
   pinMode(DOOR_ACTUATOR_RELAY_OPEN, OUTPUT);
   pinMode(DOOR_ACTUATOR_RELAY_CLOSE, OUTPUT);
 
+  // Disable both motors
   digitalWrite(DOOR_ACTUATOR_RELAY_OPEN, HIGH);
   digitalWrite(DOOR_ACTUATOR_RELAY_CLOSE,HIGH);
 
+  // Why Not?
   Serial.println( SKETCH_NAME );
 
   // Set the heartbeat timer to send the current state at the set interval
@@ -100,25 +99,11 @@ void setup() {
  * loop - The main program loop
  */
 void loop() {
-
   //Get the state of the door
   getState();
-
-  //Here we check if the state has changed and update the gateway with the change.  We do this
-  //after all processing of the state because we need to know if there was an obstruction
-  if ((lState != cState) || obstruction == true) {
+  
+  if ((lState != cState)) {
     sendCurrentState();
-
-    obstruction_count ++;
-    if (obstruction_count >= OBSTR_MAX) {
-      //Once the obstruction is checked and sent, we can clear it
-      obstruction = false;
-    }
-
-    //If the current state is full open or full closed we need to set the last limit
-    if ( (cState == OPEN) || (cState == CLOSED) ) {
-      setLastLimit( cState );
-    }
   }
 
   heartbeat.run();
@@ -133,16 +118,16 @@ void receive( const MyMessage &message ) {
   //We only expect one type of message from controller. But we better check anyway.
   if (message.type == V_STATUS){
     switch(message.getBool()){
-      case 0:
+      case CLOSED:
         // CLOSE THE DOOR
         if(cState == OPEN){
-          toggleDoor(0);
+          toggleDoor(CLOSED);
         }
         break;
-      case 1:
+      case OPEN:
         // OPEN THE DOOR
         if(cState == CLOSED){
-          toggleDoor(1);
+          toggleDoor(OPEN);
         }
         break;
     }
@@ -154,9 +139,7 @@ void receive( const MyMessage &message ) {
  * sendCurrentState - Sends the current state back to the gateway
  */
 void sendCurrentState() {
-  
-    send( msgStatus.set( obstruction == true ? "OBSTRUCTION" : currentState[cState] ) ); 
-    
+    send( msgStatus.set(cState) ); 
 } //End sendCurrentState
 
 /**
@@ -185,23 +168,10 @@ void toggleDoor(int rState) {
 } //End toggleDoor
 
 /**
- * relayOff - Used to turn off the door opener relay after the TOGGLE_INTERVAL
- */
-void relayOff() {
-  Serial.println("OFF ALL");
-  digitalWrite( DOOR_ACTUATOR_RELAY_OPEN, RELAY_OFF );
-  digitalWrite( DOOR_ACTUATOR_RELAY_CLOSE, RELAY_OFF );
-  //Added this to tell the controller that we shut off the relay
-  send( msgOpener.set(0) );
-  
-} //End relayOff
-
-/**
  * getState - Used to get the current state of the garage door.  This will set the cState variable
  *            to either OPEN, CLOSED, OPENING and CLOSING
  */
 void getState() {
-
   //read the upper sensor
   int upper = digitalRead( UPPER_LIMIT_SENSOR ); 
   //read the lower sensor
@@ -219,41 +189,5 @@ void getState() {
     //Set the current state to closed
     cState = CLOSED;
     //Check if the door is in motion
-  } else if ((upper == HIGH) && (lower == HIGH)) {
-    //If in motion and the last full position of the door was open
-    if (lLimit == OPEN) {
-      //Set the current state to closing
-      cState = CLOSING;
-      //If in motion and the last full position of the door was closed
-    } else {
-      //Set the current state to opening
-      cState = OPENING;
-    }
   }
-  
 } //End getState
-
-/**
- * setLastLimit - Checks the limit passed with the last limit (lLimit) and sets the last limit
- *                if there was a change.  It also checks the states against the limits and issues 
- *                an obstruction error if the limit and last limit
- *                
- * @param limit  integer limit - The limit to be checked
- */
-void setLastLimit( int limit ) {
-  
-  //Here is where we check for our error condition
-  if ( (lLimit == OPEN) && (limit == OPEN) && (lState == CLOSING) ) {
-    //An obstruction has reversed the door.  No need to set the last limit because it already equals
-    //the limit we are trying to set it to
-    obstruction = true;
-    //If we made it here and the last limit does not equal the limit we are setting then change it.  If
-    //the last limit is equal to the limit we are setting then the last state was something other than
-    //closing, so we don't need to do anything.
-  } else if ( lLimit != limit ) {
-    //Everything okay, set the last limit
-    lLimit = limit;
-    obstruction = false;
-  }
-  
-} //End setLastLimit
